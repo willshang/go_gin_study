888b4b94ecfc658a3950a830b91b1044f1578867
Sam <zhi1125272962@qq.com>
2019-07-08 21:50:45 +0800

    add file

--- example/12-middleware-without-middleware.go
+++ example/12-middleware-without-middleware.go
@@ -2,8 +2,7 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-// 访问http://127.0.0.1:8080/ping
-func main() {
+func main()  {
 	r := gin.New()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
--- example/13-middleware-with-default-middleware.go
+++ example/13-middleware-with-default-middleware.go
@@ -2,9 +2,8 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-// 访问http://127.0.0.1:8080/ping
-func main() {
-	r := gin.New()
+func main()  {
+	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
