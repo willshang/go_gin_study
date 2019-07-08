fd6e74de9b509aa1f3c9240c5230d9cb9386eb5b
Sam <zhi1125272962@qq.com>
2019-07-08 17:36:53 +0800

    add file

--- example/07-query+postForm.go
+++ example/07-query+postForm.go
@@ -17,9 +17,10 @@ func main() {
 		name := c.PostForm("name")
 		message := c.PostForm("message")
 
-		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+		data := fmt.Sprintf("id: %s; page: %s; name: %s; message: %s",
+			id, page, name, message)
 		c.JSON(http.StatusOK, gin.H{
-			"data": fmt.Sprintf("id: %s; page: %s; name: %s; message: %s", id, page, name, message),
+			"data": data,
 		})
 		return
 	})
