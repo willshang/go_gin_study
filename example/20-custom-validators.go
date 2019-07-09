75e4bd79bd98672dfb9ea236e3850fe7c2ba877c
Sam <zhi1125272962@qq.com>
2019-07-09 21:23:32 +0800

    add file

--- example/20-custom-validators.go
+++ example/20-custom-validators.go
@@ -13,7 +13,7 @@ import (
 // Booking包含绑定和验证的数据
 type Booking struct {
 	CheckIn  time.Time `form:"check_in" binding:"required,bookabledate" time_format:"2006-01-02"`
-	CheckOut time.Time `form:"check_out" binding:"required,gtfield=CheckIn" time_format:"2006-01-02"`
+	CheckOut time.Time `form:"check_out" binding:"required,gtfield=CheckIn" time_format:"2006-01-02"` // 大于CheckIn字段
 }
 
 func main() {
@@ -23,7 +23,8 @@ func main() {
 		v.RegisterValidation("bookabledate", bookableDate)
 	}
 
-	// localhost:8085/bookable?check_in=2018-04-16&check_out=2018-04-17
+	// http://localhost:8085/bookable?check_in=2020-04-16&check_out=2020-04-17 // 大于当前时间,验证通过
+	// http://localhost:8085/bookable?check_in=2018-03-08&check_out=2018-03-09 // 验证失败
 	route.GET("/bookable", getBookable)
 	route.Run(":8085")
 }
@@ -36,8 +37,8 @@ func getBookable(c *gin.Context) {
 		})
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-			"message":"getBookable",
+			"error":   err.Error(),
+			"message": "getBookable",
 		})
 	}
 	return
@@ -47,6 +48,7 @@ func bookableDate(v *validator.Validate, topStruct reflect.Value, currentStructO
 	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string) bool {
 	if date, ok := field.Interface().(time.Time); ok {
 		today := time.Now()
+		// 今天大于指定日期，验证失败
 		if today.After(date) {
 			return false
 		}
