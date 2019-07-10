ea1d36c688b49f0c4ebed1a87a33898bfaf77b2e
Sam <zhi1125272962@qq.com>
2019-07-10 21:47:01 +0800

    add file

---
+++ example/51-BindBodyIntoDifferentsStructs.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/prometheus/common/log"
+	"net/http"
+)
+
+type formA struct {
+	Foo string `json:"foo" xml:"foo" binding:"required"`
+}
+
+type formB struct {
+	Bar string `json:"bar" xml:"bar" binding:"required"`
+}
+
+func SomeHandlerOne(c *gin.Context) {
+	objA := formA{}
+	objB := formB{}
+	// This c.ShouldBind consumes c.Request.Body and it cannot be reused.
+	// c.ShouldBind 使用了 c.Request.Body，不可重用。
+	if errA := c.ShouldBind(&objA); errA == nil {
+		c.String(http.StatusOK, `the body should be formA`)
+		// Always an error is occurred by this because c.Request.Body is EOF now.
+		// 因为现在 c.Request.Body 是 EOF，所以这里会报错。
+	} else if errB := c.ShouldBind(&objB); errB == nil {
+		c.String(http.StatusOK, `the body should be formB`)
+	} else {
+		log.Info("SomeHandleOne")
+	}
+}
+
+func SomeHandlerTwo(c *gin.Context) {
+	objA := formA{}
+	objB := formB{}
+	// This reads c.Request.Body and stores the result into the context.
+	// 读取 c.Request.Body 并将结果存入上下文。
+	if errA := c.ShouldBindBodyWith(&objA, binding.JSON); errA == nil {
+		c.String(http.StatusOK, `the body should be formA`)
+		// At this time, it reuses body stored in the context.
+		// 这时, 复用存储在上下文中的 body。
+	} else if errB := c.ShouldBindBodyWith(&objB, binding.JSON); errB == nil {
+		c.String(http.StatusOK, `the body should be formB JSON`)
+		// And it can accepts other formats
+		// 可以接受其他格式
+	} else if errB2 := c.ShouldBindBodyWith(&objB, binding.XML); errB2 == nil {
+		c.String(http.StatusOK, `the body should be formB XML`)
+	} else {
+		log.Info("SomeHandlerTwo")
+	}
+}
